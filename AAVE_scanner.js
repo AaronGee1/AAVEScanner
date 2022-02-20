4cd0ee0caf5be55babb428d0e36dc76854fcb3f2
AaronGee1 <aaron.gee@ryerson.ca>
2022-02-20 02:43:19 +0000

    First working version of logger

--- AAVE_scanner.js
+++ AAVE_scanner.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 import util from "util";
 dotenv.config();
 
+// timeout = () => {
+//   return new Promise(resolve => setTimeout(resolve, 60))
+// }
+
 const db = await mysql.createConnection({
   host: process.env.HOST,
   user: process.env.DBUSER,
@@ -18,142 +22,117 @@ db.connect(async (err) => {
   console.log("Connected!");
 });
 
-const select = "SELECT MIN(TimeStamp) FROM AAVE_Accounts";
+const select = "SELECT MAX(TimeStamp) FROM AAVE_Accounts";
 
-const result = await db.query(select).catch((err) => {
-  console.log(err);
-});
+while (true) {
+  let result = await db.query(select).catch((err) => {
+    console.log(err);
+  });
 
-let lastTimeStamp = result[0]["MIN(TimeStamp)"];
-if (lastTimeStamp == null) {
-  lastTimeStamp = ~~(new Date().getTime() / 1000);
+  let lastTimeStamp = result[0]["MAX(TimeStamp)"];
+  if (lastTimeStamp == null) {
+    lastTimeStamp = ~~(new Date().getTime() / 1000);
+  }
+  console.log(lastTimeStamp);
+
+  let repays = gql`
+  {
+    repays(first: 100, where: { timestamp_gt: ${lastTimeStamp} }) {
+      user {
+        id
+      }
+      timestamp
+    }
+  }
+`;
+
+  let borrows = gql`
+  {
+    borrows(first: 100, where: {timestamp_gt: ${lastTimeStamp}}) {
+      user {
+        id
+      }
+      timestamp
+    }
+  }
+`;
+
+  let deposits = gql`
+  {
+    deposits(first: 100, where: {timestamp_gt: ${lastTimeStamp}}) {
+      user {
+        id
+      }
+      timestamp
+    }
+  }
+`;
+
+  const insert = "INSERT INTO AAVE_Accounts (AccountHash, TimeStamp) VALUES ?";
+
+  request(
+    "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
+    repays
+  ).then((data) => {
+    let repays = data["repays"];
+    for (const transactions in repays) {
+      let values = [
+        [repays[transactions].user.id, repays[transactions].timestamp],
+      ];
+
+      db.query(insert, [values], (err, result) => {
+        try {
+          if (err) throw err;
+          console.log(values[0][0] + " inserted");
+        } catch (err) {
+          console.log("Skipping duplicate record...");
+        }
+      });
+    }
+  });
+
+  request(
+    "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
+    borrows
+  ).then((data) => {
+    let repays = data["repays"];
+    for (const transactions in repays) {
+      let values = [
+        [repays[transactions].user.id, repays[transactions].timestamp],
+      ];
+
+      db.query(insert, [values], (err, result) => {
+        try {
+          if (err) throw err;
+          console.log(values[0][0] + " inserted");
+        } catch (err) {
+          console.log("Skipping duplicate record...");
+        }
+      });
+    }
+  });
+
+  request(
+    "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
+    deposits
+  ).then((data) => {
+    let repays = data["repays"];
+    for (const transactions in repays) {
+      let values = [
+        [repays[transactions].user.id, repays[transactions].timestamp],
+      ];
+
+      db.query(insert, [values], (err, result) => {
+        try {
+          if (err) throw err;
+          console.log(values[0][0] + " inserted");
+        } catch (err) {
+          console.log("Skipping duplicate record...");
+        }
+      });
+    }
+  });
+
+  console.log("Waiting for next Check...");
+  await new Promise((r) => setTimeout(r, 60000));
 }
-console.log(lastTimeStamp);
-
-let repays = gql`
-  {
-    repays(first: 5, where: { timestamp_lt: ${lastTimeStamp} }) {
-      user {
-        id
-      }
-      timestamp
-      amount
-      reserve {
-        name
-        symbol
-        decimals
-        liquidityRate
-        price {
-          priceInEth
-        }
-      }
-    }
-  }
-`;
-
-let borrows = gql`
-  {
-    borrows(first: 5, where: {timestamp_lt: ${lastTimeStamp}}) {
-      user {
-        id
-      }
-      timestamp
-      amount
-      reserve {
-        name
-        symbol
-        decimals
-        liquidityRate
-        price {
-          priceInEth
-        }
-      }
-    }
-  }
-`;
-
-let deposits = gql`
-  {
-    deposits(first: 5, where: {timestamp_lt: ${lastTimeStamp}}) {
-      user {
-        id
-      }
-      timestamp
-      amount
-      reserve {
-        name
-        symbol
-        decimals
-        liquidityRate
-        price {
-          priceInEth
-        }
-      }
-    }
-  }
-`;
-
-const insert = "INSERT INTO AAVE_Accounts (AccountHash, TimeStamp) VALUES ?";
-
-request(
-  "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
-  repays
-).then((data) => {
-  let repays = data["repays"];
-  for (const transactions in repays) {
-    let values = [
-      [repays[transactions].user.id, repays[transactions].timestamp],
-    ];
-
-    db.query(insert, [values], (err, result) => {
-      try {
-        if (err) throw err;
-        console.log(values[0][0] + " inserted");
-      } catch (err) {
-        console.log("Skipping duplicate record...");
-      }
-    });
-  }
-});
-
-request(
-  "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
-  borrows
-).then((data) => {
-  let repays = data["repays"];
-  for (const transactions in repays) {
-    let values = [
-      [repays[transactions].user.id, repays[transactions].timestamp],
-    ];
-
-    db.query(insert, [values], (err, result) => {
-      try {
-        if (err) throw err;
-        console.log(values[0][0] + " inserted");
-      } catch (err) {
-        console.log("Skipping duplicate record...");
-      }
-    });
-  }
-});
-
-request(
-  "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
-  deposits
-).then((data) => {
-  let repays = data["repays"];
-  for (const transactions in repays) {
-    let values = [
-      [repays[transactions].user.id, repays[transactions].timestamp],
-    ];
-
-    db.query(insert, [values], (err, result) => {
-      try {
-        if (err) throw err;
-        console.log(values[0][0] + " inserted");
-      } catch (err) {
-        console.log("Skipping duplicate record...");
-      }
-    });
-  }
-});
